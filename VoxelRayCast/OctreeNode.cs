6bf75d293e4189f2cb76f3e69c49b8d97f3340af
SameplayerDE <asameplayer@gmail.com>
2025-03-29 10:07:22 +0100

    OctreeNode.cs

---
+++ VoxelRayCast/OctreeNode.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace VoxelRayCast;
+
+public struct OctreeNode {
+    public Vector3 Min;      // Minimum des AABB
+    public Vector3 Max;      // Maximum des AABB
+    // public int ChildStart;   // Index im Buffer, ab dem die 8 Kindknoten liegen (-1, wenn Blatt)
+    // public int ChildCount;   // Anzahl der Kindknoten (0 oder 8)
+    public int Value;        // 0 = leer, 1 = voll, -1 = gemischt
+    public OctreeNode[] Children;
+}
--- VoxelRayCast/WorldGenerator.cs
+++ VoxelRayCast/WorldGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
 namespace VoxelRayCast;
 
@@ -49,5 +50,82 @@ public class WorldGenerator
             }
         }
         return chunk;
+    }
+
+    public static OctreeNode BuildOctree(Chunk chunk)
+    {
+        return BuildOctree(chunk.Data, 0, 0, 0, Chunk.Size);
+    }
+
+    private static OctreeNode BuildOctree(int[] data, int x, int y, int z, int size)
+    {
+        var result = new OctreeNode
+        {
+            Min = new Vector3(x, y, z),
+            Max = new Vector3(x + size, y + size, z + size)
+        };
+
+        if (IsHomogeneous(data, x, y, z, size, out var value))
+        {
+            result.Value = value;
+            result.Children = null;
+        }
+        else
+        {
+            result.Value = -1;
+            if (size > 1)
+            {
+                var half = size / 2;
+                result.Children = new OctreeNode[8]; // has always 8 childs if not homo (haha, homo)
+                
+                int index = 0;
+                for (int dx = 0; dx < 2; dx++)
+                {
+                    for (int dy = 0; dy < 2; dy++)
+                    {
+                        for (int dz = 0; dz < 2; dz++)
+                        {
+                            result.Children[index++] = BuildOctree(
+                                data,
+                                x + dx * half,
+                                y + dy * half,
+                                z + dz * half,
+                                half);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                result.Children = null;
+            }
+        }
+        
+        return result;
+    }
+    
+    private static int GetVoxel(int[] data, int x, int y, int z)
+    {
+        return data[x + Chunk.Size * (z + Chunk.Size * y)];
+    }
+    
+    private static bool IsHomogeneous(int[] data, int x0, int y0, int z0, int size, out int value)
+    {
+        value = GetVoxel(data, x0, y0, z0);
+        for (int y = 0; y < size; y++)
+        {
+            for (int z = 0; z < size; z++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    int current = GetVoxel(data, x0 + x, y0 + y, z0 + z);
+                    if (current != value)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
